07f3d80305fe20a34eec2afb0fc78b3bb6b7a56c
Chamsol Yoon <dantert@gmail.com>
2021-08-02 15:38:32 +0900

    회원 도메인 개발

---
+++ src/main/java/io/hkarling/service/MemberService.java
@@ -0,0 +1,45 @@
+package io.hkarling.service;
+
+import io.hkarling.domain.Member;
+import io.hkarling.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+//    @Autowired
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    @Transactional
+    public long join(Member member) {
+        validateDuplicatedMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicatedMember(Member member) {
+        List<Member> findMember = memberRepository.findByName(member.getName());
+        if(!findMember.isEmpty()) {
+            throw new IllegalStateException("Member already exists");
+        }
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
---
+++ src/test/java/io/hkarling/service/MemberServiceTest.java
@@ -0,0 +1,62 @@
+package io.hkarling.service;
+
+import io.hkarling.domain.Member;
+import io.hkarling.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
+
+    @Test
+    public void join() {
+        // given
+        Member member = new Member();
+        member.setName("NAME_A");
+
+        // when
+        Long savedId = memberService.join(member);
+
+        // then
+        em.flush();
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test
+    public void validateDuplicatedMember() {
+        // given
+        Member member1 = new Member();
+        member1.setName("MEMBER_A");
+
+        Member member2 = new Member();
+        member2.setName("MEMBER_A");
+
+        // when
+        memberService.join(member1);
+//        try {
+//            memberService.join(member2);
+//        } catch (IllegalStateException e) {
+//            return;
+//        }
+
+        // then
+        //fail("EXCEPTION OCCURS");
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+    }
+
+}
